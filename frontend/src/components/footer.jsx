0c3a522df5b53e2dc9ef686c631a7101819dc3a6
amalekia <amalekia@calpoly.edu>
2024-11-17 18:57:47 -0800

    more styling done

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import "../styles/footer.css";
 
 export default function Footer() {
-    return (
-        <footer className="bg-light py-4">
-            <div className="container text-center">
-                <p className="mb-2">
-                    &copy; {new Date().getFullYear()} StarrCars. All rights reserved.
-                </p>
-                <ul className="list-inline">
-                    <li className="list-inline-item">
-                        <Link to="/about" style={{color: "black"}}>About Us</Link>
-                    </li>
-                    <li className="list-inline-item">
-                        <Link to="/contact" style={{color: "black"}}>Contact</Link>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-    );
-};
+  return (
+    <footer className="footer">
+      <div className="container text-center">
+        <p className="mb-2">
+          &copy; {new Date().getFullYear()} StarrCars. All rights reserved.
+        </p>
+        <ul className="list-inline">
+          <li className="list-inline-item">
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="list-inline-item">
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
--- frontend/src/views/about.jsx
+++ frontend/src/views/about.jsx
@@ -4,18 +4,19 @@ import "../styles/about.css";
 
 export default function AboutPage() {
     return (
-        <div classname="aboutpage">
+        <div className="aboutpage">
             <h1>About Us</h1>
             <p>
-                At StarrCars, we are dedicated to helping our clients confidently buy and sell their vehicles. 
-                Our team of experts is here to assist you with any questions you may have, making the process 
-                of buying and selling cars as easy as possible. Our tools are designed to help you make the 
-                best decisions for your vehicle transactions. Feel free to reach out to us with any questions 
-                you may have below!
+                At StarrCars, we are dedicated to helping our clients confidently buy and sell their vehicles.
+                Our team of experts is here to assist you with any questions you may have, making the process
+                of buying and selling cars as seamless as possible. Our tools are designed to help you make
+                the best decisions for your vehicle transactions.
+            </p>
+            <p>
+                Feel free to reach out to us with any questions by clicking the link below!
             </p>
             <Link to="/contact">Click here to contact us</Link>
             <h4>Thank you for choosing StarrCars!</h4>
         </div>
-    )
+    );
 }
-
